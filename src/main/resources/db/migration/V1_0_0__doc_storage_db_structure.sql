78526de5ca323c54a3de110e6488a08c180e9a4f
RuslanYapparov <ruyappy@yandex.ru>
2025-01-30 22:18:14 +0400

    feat: Configure Flyway migrations on startup. And linters.

--- src/main/resources/db/migration/V1_0_0__doc_storage_db_structure.sql
+++ src/main/resources/db/migration/V1_0_0__doc_storage_db_structure.sql
@@ -1,16 +1,10 @@
-IF NOT EXISTS (
-    SELECT *
-    FROM sys.objects
-    WHERE object_id = OBJECT_ID(N'[dbo].[users]')
-    AND type = N'U'
-)
-BEGIN
-    CREATE TABLE [dbo].[users] (
-        user_id BIGINT IDENTITY(1,1) PRIMARY KEY,
-        username NVARCHAR(255) NOT NULL UNIQUE,
-        password NVARCHAR(255) NOT NULL,
-        email NVARCHAR(255) NOT NULL UNIQUE,
-        first_name NVARCHAR(255),
-        last_name NVARCHAR(255)
-    );
-END
+CREATE TABLE users (
+    user_id BIGINT IDENTITY(1,1) PRIMARY KEY,
+    username NVARCHAR(20) NOT NULL UNIQUE,
+    password NVARCHAR(255) NOT NULL,
+    email NVARCHAR(255) NOT NULL UNIQUE,
+    first_name NVARCHAR(50),
+    last_name NVARCHAR(50)
+);
+
+CREATE INDEX idx_users_username ON users (username);
