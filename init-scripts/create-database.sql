30a1c45662e0be5bf2f2df9f88b8664e3418d139
Ruslan Yapparov <RuYappy@yandex.ru>
2025-02-12 20:32:33 +0400

    chore: Accept changes from amend commit.

--- init-scripts/create-database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE docstorage_db
-COLLATE Russian_CI_AS;
-
-USE docstorage_db;
-
-CREATE LOGIN docstorage_dev WITH PASSWORD = 'ruyappy777';
-CREATE USER docstorage_dev FOR LOGIN docstorage_dev;
-
-ALTER ROLE db_owner ADD MEMBER docstorage_dev;
